e5ba264a5aea5d8a8f720daced1eeda08f5b2a6b
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2023-09-06 05:05:02 +0200

    work on custom precise engine access implementation (unfinished)

--- src/logic/audio_systems/_precise_predictor.py
+++ src/logic/audio_systems/_precise_predictor.py
@@ -1,26 +1,57 @@
 import subprocess
+from typing import List, Callable
+
 
 class PrecisePredictorObservable:
     _instance = None
     _is_initialized = False
     is_paused = False
+    observers: List[Callable[[str], None]] = []
+    process = None
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __init__(self, engine_path, model_path):
+    def __init__(self, engine_path: str, model_path: str, chunk_size: int):
         # singleton
         if self._is_initialized:
             return
 
         # Init pipeline
-        io.get
-        process = subprocess.Popen(['grep', 'hello'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        self.engine_path = engine_path
+        self.model_path = model_path
+        self.chunk_size = chunk_size
+        self.process = None
 
     def pause(self):
         self.is_paused = True
 
     def unpause(self):
         self.is_paused = False
+
+    def feed(self, data):
+        if self.is_paused:
+            return
+        if self.process is not None:
+            self.process.stdin.write(data)
+
+    def subscribe(self, func: Callable[[str], None]) -> None:
+        self.observers.append(func)
+
+    def _spawn_process(self):
+        self.process = subprocess.Popen(
+            [self.engine_path, self.model_path, str(self.chunk_size)],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+
+    def notify_all(self, data: str) -> None:
+        for subscriber in self.observers:
+            subscriber(data)
+
+    def _debug_eval(self):
+        self.process.stdin.close()
+        stdout_data, stderr_data = self.process.communicate()
+        self.notify_all(stdout_data.decode('utf-8'))
--- src/trystuff.py
+++ src/trystuff.py
@@ -1,10 +1,20 @@
+import logic.audio_systems._precise_predictor as pp
 import logic.audio_systems.wakeword_access as precise
 import logic.audio_systems.audio_experiments as rectest
+import logic.utils.io_access as io
 
 # -- Wake word
-precise.initialize_precise()
-precise.start_wakeword_detection(lambda x: print(x), lambda: print("active"))
+# precise.initialize_precise()
+# precise.start_wakeword_detection(lambda x: print(x), lambda: print("active"))
 
 # -- Record Sound
-#rectest.list_audio_devices_and_rates()
-#rectest.record_wav_test()
+# rectest.list_audio_devices_and_rates()
+# rectest.record_wav_test()
+
+
+precise_observer = pp.PrecisePredictorObservable(
+    io.get_path('data', 'precise-engine', 'precise-engine'),
+    io.get_path('data', 'precise-models', 'hey-mycroft-en.pb'),
+    2048
+)
+precise_observer.subscribe(lambda x: print(x))
